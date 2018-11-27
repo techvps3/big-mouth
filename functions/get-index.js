8aa0e7d8d000f56667c0e6d6158f6ce09c5e81cf
haynesi <admin@vipi.io>
2018-11-26 19:44:54 -0500

    Adding more code

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,29 +1,41 @@
 'use strict';
 
 const co = require("co");
-const Promis = require("bluebird");
-const fs = Promis.promisifyAll(require("fs"));
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require('mustache');
+const http = require('superagent-promise')(require('superagent'), Promise);
+
+const restaurantsApiRoot = process.env.restaurants_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var html;
 
 function* loadHtml() {
-  if(!html) {
-    html = yield fs.readFileAsync('static/index.html','utf-8');
+  if (!html) {
+    html = yield fs.readFileAsync('static/index.html', 'utf-8');
   }
+
   return html;
 }
 
-module.exports.handler = co.wrap(function* (event, context,callback)  {
-  let html = yield loadHtml();
+function* getRestaurants() {
+  return (yield http.get(restaurantsApiRoot)).body;
+}
+
+module.exports.handler = co.wrap(function* (event, context, callback) {
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, { dayOfWeek, restaurants });
+
   const response = {
     statusCode: 200,
-    body:html,
+    body: html,
     headers: {
       'Content-Type': 'text/html; charset=UTF-8'
     }
   };
 
-  callback(null,response)
-
-
+  callback(null, response);
 });
